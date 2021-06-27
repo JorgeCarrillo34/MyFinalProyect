15c1b51f8eb847591fbb81296cd34f2bbd6841e3
Jorge Carrillo <jorgecardi00@gmail.com>
2021-06-27 17:12:03 -0500

    Version 4: Se agrego mas botones para interconectar todas las pantallas

--- app/src/main/java/co/edu/myfinalproyect/LoginActivity.java
+++ app/src/main/java/co/edu/myfinalproyect/LoginActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     public void registro(View view){
@@ -20,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void registro1(View view){
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
