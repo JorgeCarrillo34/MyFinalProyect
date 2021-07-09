25ae1f1a401a95df753563565c206c106afe2cbe
santiago <amayas929@gmail.com>
2021-07-08 23:43:04 -0500

    VERSION 6
    se agregan iconos a toolbar

--- app/src/main/java/co/edu/myfinalproyect/TipoDeCargaActivity.java
+++ app/src/main/java/co/edu/myfinalproyect/TipoDeCargaActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,6 +21,7 @@ public class TipoDeCargaActivity extends AppCompatActivity {
     ListView listaCarga;
     Toolbar toolbar;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
